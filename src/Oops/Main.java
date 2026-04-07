8e73e49fef4da71282bd351c4e0386c88abef99e
Deepa M <deepa.madagudi@isteer.com>
2026-04-07 18:30:27 +0530

    Data types and Encapsulation

--- src/Oops/Main.java
+++ src/Oops/Main.java
@@ -8,8 +8,14 @@ public class Main {
 
         student.setName("Deepa");
         student.setAge(25);
+        int num = 10;
+        double convertedNum = num;
+
+        System.out.println("Implicit Casting (int to double): " + convertedNum);
 
         System.out.println("Name: " + student.getName());
         System.out.println("Age: " + student.getAge());
+        System.out.println("Num: " + num);
+        System.out.println("Converted Num: " + convertedNum);
     }
 }
