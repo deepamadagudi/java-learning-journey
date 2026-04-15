9481b0ba969d151c65208ac5ccdd058ba77c72c7
Deepa M <deepa.madagudi@isteer.com>
2026-04-15 17:28:42 +0530

    feat(OOP): add Inheritance with Ex

---
+++ src/Oops/Ex1Main.java
@@ -0,0 +1,11 @@
+package Oops;
+
+public class Ex1Main {
+
+        public static void main(String[] args) {
+            Car myCar = new Car();
+            myCar.start();
+            myCar.stop();
+            myCar.honk();
+        }
+    }
--- src/Oops/MainStud.java
+++ src/Oops/MainStud.java
@@ -1,6 +1,6 @@
 package Oops;
 
-public class Main {
+public class MainStud {
 
     public static void main(String[] args) {
 
